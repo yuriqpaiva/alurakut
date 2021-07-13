4b6ac7a5f424f98406d068ed0cc418aed84ce08b
yuriqpaiva <yuriqpaiva@gmail.com>
2021-07-12 21:40:41 -0300

    Primeiro dia imersão
    Foram criadas as principais estruturas do site, colunas no CSS  e adicionadas alguns padrões de CSS, assim como alguns conteúdos gerados através do JS pegando valores na API do GitHub  e trazendo para o HTML.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,71 @@
 import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { Fragment } from 'react'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+function ProfileSidebar(props) {
+  console.log(props)
+  return (
+    <Box>
+      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }}></img>
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = 'yuriqpaiva'
+  const pessoasFavoritas = [
+    'juunegreiros',
+    'omariosouto',
+    'peas',
+    'rafaballerini',
+    'marcobrunodev',
+    'felipefialho'
+  ]
+
+  return (
+    <Fragment>
+      <AlurakutMenu />
+      <MainGrid >
+        <div className='profileArea' style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar githubUser={githubUser} />
+        </div>
+        <div className='welcomeArea' style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1 className='title'>
+              Bem vindo(a)
+            </h1>
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+        <div className='profileRelationsArea' style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className='smallTitle'>
+              Pessoas da comunidade ({pessoasFavoritas.length})
+            </h2>
+            {/* Map retorna itens! Por isso não pode ser usado forEach */}
+            <ul>
+              {pessoasFavoritas.map((itemAtual) => {
+                return(
+                  <li>
+                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                      <img src={`http://github.com/${itemAtual}.png`}></img>
+                      <span>{itemAtual}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </Fragment>
+  )
 }
