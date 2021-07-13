c8757f1d38439f1dd5173da16aff2400cfc6ea37
yuriqpaiva <yuriqpaiva@gmail.com>
2021-07-13 20:15:23 -0300

    Implementado o formulário

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import styled from 'styled-components'
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
@@ -6,13 +7,15 @@ import { Fragment } from 'react'
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
 import { ProfileSidebar } from '../src/components/ProfileSidebar'
 
-// const Title = styled.h1`
-//   font-size: 50px;
-//   color: ${({ theme }) => theme.colors.primary};
-// `
-
 export default function Home() {
   const githubUser = 'yuriqpaiva'
+  const [comunidades, setComunidades] = React.useState([{
+    id: '21312872734728387492',
+    title: 'Eu odeio acordar cedo',
+    image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
+  }])
+  console.log(comunidades)
+  // const comunidades = ['Alurakut']
   const pessoasFavoritas = [
     'juunegreiros',
     'omariosouto',
@@ -27,7 +30,7 @@ export default function Home() {
       <AlurakutMenu />
       <MainGrid >
         <div className='profileArea' style={{ gridArea: 'profileArea' }}>
-          <ProfileSidebar githubUser={githubUser} />
+          <ProfileSidebar githubUser={githubUser} as="aside"/>
         </div>
         <div className='welcomeArea' style={{ gridArea: 'welcomeArea' }}>
           <Box>
@@ -36,8 +39,59 @@ export default function Home() {
             </h1>
             <OrkutNostalgicIconSet />
           </Box>
+          <Box>
+            <h2 className='subTitle'>O que você deseja fazer?</h2>
+            <form onSubmit={function handleCriarComunidade(e) {
+              const dadosDoForm = new FormData(e.target) // Dados do Form
+
+              const comunidade = {
+                id: new Date().toISOString(),
+                titulo: dadosDoForm.get('title'),
+                image: dadosDoForm.get('image')
+              }
+              e.preventDefault()
+              const comunidadesAtualizadas = [...comunidades, comunidade]
+              setComunidades(comunidadesAtualizadas)
+              console.log(comunidades)
+            }}>
+              <div>
+                <input
+                  placeholder='Qual vai ser o nome da sua comunidade?'
+                  name='title'
+                  aria-label='Qual vai ser o nome da sua comunidade?'
+                  type='text'
+                />
+              </div>
+              <div>
+                <input
+                  placeholder='Coloque uma URL para usarmos de capa'
+                  name='image'
+                  aria-label='Coloque uma URL para usarmos de capa'
+                />
+              </div>
+              <button>
+                Criar comunidade
+              </button>
+            </form>
+          </Box>
         </div>
         <div className='profileRelationsArea' style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBoxWrapper>
+            <h2>Comunidades</h2>
+            {/* Map retorna itens! Por isso não pode ser usado forEach */}
+            <ul>
+              {comunidades.map((itemAtual) => {
+                return (
+                  <li key={itemAtual.id}>
+                    <a href={`/users/${itemAtual.title}`}>
+                      <img src={itemAtual.image}></img>
+                      <span>{itemAtual.title}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
           <ProfileRelationsBoxWrapper>
             <h2 className='smallTitle'>
               Pessoas da comunidade ({pessoasFavoritas.length})
@@ -45,8 +99,8 @@ export default function Home() {
             {/* Map retorna itens! Por isso não pode ser usado forEach */}
             <ul>
               {pessoasFavoritas.map((itemAtual) => {
-                return(
-                  <li>
+                return (
+                  <li key={itemAtual}>
                     <a href={`/users/${itemAtual}`} key={itemAtual}>
                       <img src={`http://github.com/${itemAtual}.png`}></img>
                       <span>{itemAtual}</span>
--- src/components/ProfileSidebar/index.js
+++ src/components/ProfileSidebar/index.js
@@ -1,9 +1,18 @@
 import Box from "../Box"
+import { AlurakutProfileSidebarMenuDefault } from '../../lib/AlurakutCommons'
 
 export function ProfileSidebar(props) {
     return (
         <Box>
             <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }}></img>
+            <hr />
+            <p>
+                <a className='boxLink' href={`https://github.com/${props.githubUser}`}>
+                    @{props.githubUser}
+                </a>
+            </p>
+            <hr />
+            <AlurakutProfileSidebarMenuDefault />
         </Box>
     )
 }
